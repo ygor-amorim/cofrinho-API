3744b72d22b6d6d24bc0946fd65c39aabea03bc9
Ygor Amorim <ygoramorim3@hotmail.com>
2025-12-26 11:26:35 +0000

    Refactor error handler to ignore unused parameters

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,5 @@
-module.exports = (err, req, res, next) => {
+// eslint-disable-next-line no-unused-vars
+module.exports = (err, _req, res, _next) => {
     console.error(err);
     const status = err.status || 500;
     res.status(status).json({ error: err.message })
